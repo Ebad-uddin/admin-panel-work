a6d038630f020db39da0809472d706680c0c06f2
ebad-uddin <ebad@gmail.com>
2023-09-11 11:54:01 +0500

    admin panel user addition updated

---
+++ adduser.php
@@ -0,0 +1,34 @@
+<?php
+include('includes/config.php');
+
+if(isset($_POST['adduser'])){
+    $name = mysqli_real_escape_string($connection,$_POST['name']);
+    $email = mysqli_real_escape_string($connection,$_POST['email']);
+    $pass = mysqli_real_escape_string($connection,$_POST['pass']);
+    $cpass = mysqli_real_escape_string($connection,$_POST['cpass']);
+    $phone = mysqli_real_escape_string($connection,$_POST['phone']);
+
+    if($pass == $cpass){
+        $encpass = password_hash($pass, PASSWORD_BCRYPT);
+        $email_check = "SELECT * from userss where email = '$email'";
+        $email_checkdb = mysqli_query($connection, $email_check);
+        if(mysqli_num_rows($email_checkdb) > 0){
+            ?>
+<script>
+    alert('email already exist');
+</script>
+            <?php
+        }else{
+            $insertquery = "INSERT INTO `userss` (`name`, `email`, `password`, `phone`) VALUES ('$name', '$email', '$encpass', '$phone')
+            ";
+            $result = mysqli_query($connection,$insertquery);
+            if($result){
+                header('location: registeredusers.php');
+            }
+        }
+    }else{
+        echo "pass and cpass must match";
+    }
+}
+
+?>
